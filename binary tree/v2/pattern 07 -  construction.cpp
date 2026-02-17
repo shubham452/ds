c9ebd0b7b8c8ca27218f41887850d24df30b3253
shubham452 <55233300+shubham452@users.noreply.github.com>
2026-02-17 21:17:04 +0530

    Update pattern 07 -  construction.cpp

--- binary tree/v2/pattern 07 -  construction.cpp
+++ binary tree/v2/pattern 07 -  construction.cpp
@@ -38,3 +38,51 @@ class Solution {
     }
 };
 ---------------------------------------------------------------------------------
+105. Construct Binary Tree from Preorder and Inorder Traversal
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+
+  /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //we are using index because we don't know which root will come next at what index
+    int index=0;
+    unordered_map<int,int> mp;
+
+    TreeNode *construct(vector<int> preorder, int start, int end)
+    {
+        if(start>end)
+        {
+            return NULL;
+        }
+        //picking the first preorder array and fixing it to root because in preorder first one is root
+        TreeNode *root = new TreeNode(preorder[index++]);
+        //finding the index of inorder mid to know how many are in left subtree and hw many are in right subtree
+        int mid = mp[root->val];
+
+        //creating the tree
+        root->left = construct(preorder, start,mid-1);
+        root->right = construct(preorder, mid+1,end);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
+    {
+        
+        // we are storing inorder index in map to know how many are in left and right so that we can pass that number in preorder
+        for(int i=0;i<inorder.size();i++)
+        {
+            mp[inorder[i]]=i;
+        }
+        
+        return construct(preorder,0,inorder.size()-1);
+    }
+};
