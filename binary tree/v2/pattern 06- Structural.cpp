7a075f128c7c40550297aa26bb68873e7a4f4a05
shubham452 <55233300+shubham452@users.noreply.github.com>
2026-02-19 20:05:47 +0530

    Update pattern 06- Structural.cpp

--- binary tree/v2/pattern 06- Structural.cpp
+++ binary tree/v2/pattern 06- Structural.cpp
@@ -19,3 +19,36 @@ public:
         return root1;
     }
 };
+
+---------------------------------------------------------------------------------------
+226. Invert Binary Tree
+https://leetcode.com/problems/invert-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        TreeNode *temp = new TreeNode;
+        temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
